9e0e512030467c6f338b73d3993e353f4d6b2cc0
Vahe <vahe.aslanyan@gmail.com>
2019-09-23 23:58:07 +0000

    delete temporary adding user

--- app.js
+++ app.js
@@ -59,9 +59,6 @@ app.use(function(req, res, next){
 	next();
 });
 
-
-	var newUser = new User({username: "Levon"});
-	User.register(newUser, "Melod0374arM")
 //using routes
 app.use(artsRoutes);
 app.use(indexRoutes);
